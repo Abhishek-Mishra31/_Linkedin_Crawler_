38e856a5e863091a1a9a9d449b2b463e1de04acf
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2025-03-31 22:01:33 +0530

    updated_with_cloud_databases

--- Backend/Db.js
+++ Backend/Db.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const dbServerUrl = process.env.DB_SERVER_URL;
+const liveServerUrl = process.env.DB_LIVE_SERVER_URL;
 const db = mongoose.connection;
 
-console.log("db server url is " + dbServerUrl);
+console.log("db server url is " + liveServerUrl);
 mongoose.connect(dbServerUrl);
 
 db.on("connected", () => {
