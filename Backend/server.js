0ec25d2e1242a5390e55444134af6bbd3e3cafc2
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2025-05-20 00:11:13 +0530

    headless_fixed

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,7 @@ app.post("/scrape", async (req, res) => {
   try {
     console.log("Launching Puppeteer...");
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
