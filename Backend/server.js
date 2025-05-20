ade4f02bc2421c91d88bca9f57d5c68cdfb40f39
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2025-05-20 20:30:26 +0530

    fixed_agent

--- Backend/server.js
+++ Backend/server.js
@@ -56,6 +56,10 @@ app.post("/scrape", async (req, res) => {
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
     );
+
+    await page.setExtraHTTPHeaders({
+      "Accept-Language": "en-US,en;q=0.9",
+    });
     console.log("Loading cookies...");
     await loadCookies(page);
     console.log("Cookies loaded successfully.");
