59f1b16d02fe48c0b61c6c1ca9b0b3e2cee89adf
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2025-05-20 21:14:54 +0530

    runtime_error_fix

--- Backend/server.js
+++ Backend/server.js
@@ -44,12 +44,14 @@ app.post("/scrape", async (req, res) => {
   try {
     console.log("Launching Puppeteer...");
     browser = await puppeteerExtra.launch({
-      headless: true,
+      headless: "new",
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
         "--disable-dev-shm-usage",
         "--disable-blink-features=AutomationControlled",
+        "--disable-gpu",
+        "--window-size=1920,1080",
       ],
     });
 
